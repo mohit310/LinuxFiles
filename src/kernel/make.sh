95c06bed5c3c7f653a5f8baa30e02513b57ab1e5
Mohit310 <mohit310@users.noreply.github.com>
2023-06-28 21:26:45 -0400

    Updated

--- src/kernel/make.sh
+++ src/kernel/make.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-linux_version=linux-5.18.14
-filename=${linux_version}.tar.xz
-if [ ! -d linux ]; then
-    echo "Downloading kernel $linux_version"
-    curl -O https://cdn.kernel.org/pub/linux/kernel/v5.x/$filename
-    echo "Finished downloading"
-    if [ ! -f "$filename" ]; then
-       echo "Linux file $filename does not exist"
+new_version=6.3.10
+new_full_version=linux-$new_version
+file_name=${new_full_version}.tar.xz
+if [ ! -f $file_name ]; then
+    echo "Downloading kernel $new_version"
+    major_version=`echo $new_version | cut -d'.' -f1`
+    file_name=${new_full_version}.tar.xz
+    echo $file_name
+    full_path="https://cdn.kernel.org/pub/linux/kernel/v${major_version}.x/${file_name}"
+    curl -O $full_path
+    echo "Finished downloading $file_name"
+    if [ ! -f "$file_name" ]; then
+       echo "Linux file $file_name does not exist"
        exit 1   
     fi
     echo "extracting file"
-    tar -xf $filename
-    echo "setting up linux link"
-    ln -sf $linux_version linux
+    tar -xf $file_name
+fi
+symlink_point_to_dir=`readlink linux`
+current_version=`echo $symlink_point_to_dir | cut -d'-' -f2`
+if [ "$current_version" != "$new_version" ]; then
+    echo "setting up linux link to $new_version from $current_version"
+    rm -rf linux
+    ln -sf $new_full_version linux
 fi
 echo "changing to linux dir"
 cd linux
@@ -27,14 +37,14 @@ if [ $? -eq 0 ]; then
     patch -p1 < $patchfile
 fi    
 echo "making kernel"
-scripts/config -e LTO_CLANG_FULL
-make LLVM=1 LLVM_IAS=1 CC=clang oldconfig
-make LLVM=1 LLVM_IAS=1 CC=clang -j16
-sudo make LLVM=1 LLVM_IAS=1 CC=clang -j16 modules_install 
+#scripts/config -e LTO_CLANG_FULL
+#make LLVM=1 LLVM_IAS=1 CC=clang oldconfig
+#make LLVM=1 LLVM_IAS=1 CC=clang -j16
+#sudo make LLVM=1 LLVM_IAS=1 CC=clang -j16 modules_install 
 
-#make menuconfig
-#make -j16
-#sudo make -j16 modules_install 
+make menuconfig
+make -j14
+sudo make -j16 modules_install 
 echo "copying kernel"
 sudo cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux-mk
 echo "initcpio"
